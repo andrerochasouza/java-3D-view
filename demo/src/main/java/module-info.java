fbb188992a6788dd353909cf554146b208c06128
André da Rocha Souza <58944953+andrerochasouza@users.noreply.github.com>
2024-09-17 18:41:27 -0300

    Revert "JavaFX"
    This reverts commit 37ea21796ff28cfbaafbfff029de4e1a22014d7f.

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.com.andre.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens br.com.andre.demo to javafx.fxml;
-    exports br.com.andre.demo;
-}
---
+++ java3dview/src/main/java/br/com/andre/Main.java
@@ -0,0 +1,9 @@
+package br.com.andre;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
